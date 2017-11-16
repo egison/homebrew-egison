a278545af7a3b602df5189c483be74d3e30d85ee
greymd <greengregson@gmail.com>
2017-11-16 12:40:11 +0900

    Update version number

--- egison.rb
+++ egison.rb
@@ -5,7 +5,7 @@ class Egison < Formula
   head "https://github.com/egison/egison.git", :branch => "master"
 
   resource "egison_bin" do
-    url "https://github.com/egison/homebrew-egison/releases/download/v3.7.0/egison_darwin_amd64.zip"
+    url "https://github.com/egison/homebrew-egison/releases/download/3.7.0-1/egison_darwin_amd64.zip"
     sha256 "11e6a76ca3bec4b6e9a1f2c37fd0bee66e6a01b426f17ca942e7f71ff9b2c9ec"
   end
 
