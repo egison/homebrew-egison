6e2a062e489a9bffc9b8c44b16797291b50fb139
greymd <greengregson@gmail.com>
2017-12-03 17:56:39 +0900

    [skip ci] Update sha256

--- egison.rb
+++ egison.rb
@@ -3,7 +3,7 @@ class Egison < Formula
   desc "A purely functional programming language with non-linear pattern-matching against non-free data types"
   homepage "https://www.egison.org/"
   url "https://github.com/egison/homebrew-egison/releases/download/#{VERSION}/egison_darwin_x86_64.tar.gz"
-  sha256 "d39a5e656e2b3f62c5e331cb9457965c6b3116afd1db8abca889b7b5f6cdc821"
+  sha256 "1b7a70c27bc21b2067cde2f340add4bffd7d592bec73cbdb8386530c49614e45"
 
   def install
     bin.install "bin/egison"
