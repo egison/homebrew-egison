ea04d4af93a9de8fb55f4a8413ee95908c88f032
greymd <greengregson@gmail.com>
2019-08-17 00:55:51 +0100

    Update Paths_egison.hs

--- release.sh
+++ release.sh
@@ -90,14 +90,18 @@ bump () {
 }
 
 build () {
-  local _exefile
+  local _exefile _pathsfile
   local _workdir="work-$RANDOM"
   git clone -b "${LATEST_VERSION}" \
     "https://github.com/${BUILD_REPO}.git" "${THIS_DIR}/egison"
   cd "${THIS_DIR}/egison"
   cabal v2-update
   cabal v2-install --only-dependencies --lib
-  cabal v2-configure --datadir=/usr/local/lib --datasubdir=egison
+  cabal v2-configure
+  cabal v2-build
+  _pathsfile="$(find "${THIS_DIR}/egison/dist-newstyle" -type f -name 'Paths_egison.hs' | head -n 1)"
+  perl -i -pe 's@datadir[ ]*=[ ]*.*$@datadir = "/usr/local/lib/egison"@' "$_pathsfile"
+  cp "$_pathsfile" "${THIS_DIR}/egison/hs-src"
   cabal v2-build
   mkdir -p "${_workdir}/bin"
   mkdir -p "${_workdir}/lib/egison"
