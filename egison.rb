c1649484e2a7bf7439ae34cebb6b1ed9b74b12e4
greymd <yamadagrep@gmail.com>
2020-06-08 00:30:15 +0000

    [skip ci] Bump version to 4.0.3

--- egison.rb
+++ egison.rb
@@ -1,10 +1,10 @@
 class Egison < Formula
-  VERSION = "4.0.1"
+  VERSION = "4.0.3"
   version VERSION
   desc "A purely functional programming language with non-linear pattern-matching against non-free data types"
   homepage "https://www.egison.org/"
   url "https://github.com/egison/homebrew-egison/releases/download/#{VERSION}/egison_darwin_x86_64_#{VERSION}.zip"
-  sha256 "aa6c276aadc1a958f3942174dd9860cba84f68549e005ce61b56a8abc3c93a74"
+  sha256 "36d64e1c8cf0762f9fedc74904f3b2637936857f125d2947bc0bffbf97b8d90b"
 
   def install
     bin.install "bin/egison"
